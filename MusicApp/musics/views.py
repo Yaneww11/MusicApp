1215a47288bb40334c7587ddfbdf1db9d0b9f7af
Yaneww11 <93374652+Yaneww11@users.noreply.github.com>
2024-08-21 20:59:21 +0300

    Config settings.py, add templates and static directories

--- MusicApp/musics/views.py
+++ MusicApp/musics/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
+from MusicApp.common.session_decorator import session_decorator
+from MusicApp.musics.models import Album
+from MusicApp.settings import session
+
+
 # Create your views here.
+@session_decorator(session)
+def index(request):
+    albums = session.query(Album).all()
+
+    context = {
+        'albums': albums,
+    }
+    return render(request, 'common/index.html', context)
+
+
+def create_album(request):
+    return render(request, 'albums/create-album.html')
+
+
+def details_album(request, id: int):
+    return render(request, 'albums/details-album.html')
+
+
+def edit_album(request, id: int):
+    return render(request, 'albums/edit-album.html')
+
+
+def delete_album(request, id: int):
+    return render(request, 'albums/delete-album.html')
+
+
+def create_song(request):
+    return render(request, 'songs/create-song.html')
--- alembic/env.py
+++ alembic/env.py
@@ -2,10 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
-
 from MusicApp.settings import Base
+from MusicApp.musics.models import Album, Song
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
